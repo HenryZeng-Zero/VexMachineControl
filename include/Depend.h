a28b50223b2f56706ba439ff78c0195ea2959992
Henry Zeng <z13405868569@163.com>
2020-10-05 13:53:32 +0800

    一堆辣鸡

--- include/Depend.h
+++ include/Depend.h
@@ -1,5 +1,6 @@
 #include "vex.h"
-using namespace std;
+#include "cmath"
+using namespace vex;
 
 class InertialTool{
     public:
@@ -30,24 +31,36 @@ class InertialTool{
             }
         }
         
-        void TurningLimit(bool Direction,double deg,int power_pct){
-            double angle = Direction_preprocessing(Direction,deg);
+        void BackDistance(bool Direction,double deg){
+            double angle = Inertial.angle();
+
             if(Direction){
                 if(angle + deg > 360){
                     // 特别判断 ： 逆时针 越过 0
-                    return (deg - (360 - angle));
-                }else{
-                    return (angle + deg);
-                }
-            }else{
-                
-                if((angle -0) < deg){
-                    // 特别判断 ： 顺时针 越过 0
-                    return (360 - (deg - (angle -0)));
+                    return (deg + 360 - angle);
                 }else{
                     return (angle - deg);
                 }
+            }else{
+                
+                // if((angle -0) < deg){
+                //     // 特别判断 ： 顺时针 越过 0
+                //     return (360 - (deg - (angle -0)));
+                // }else{
+                //     return (angle - deg);
+                // }
             }
+        }
+
+        void AngleDistance(double deg){
+            double angle_plus = Direction_preprocessing(true,deg);
+            double angle_minus = Direction_preprocessing(false,deg);
+            return min(angle_plus,angle_minus);
+        }
+
+        void Turning_Limit(bool Direction,double deg,int power_pct){
+            double angle = Direction_preprocessing(Direction,deg);
+            
         }
 
 };
